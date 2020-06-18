1a309227f1b4b4b3fbebc5765dbb80b4556124a7
Jessica Pereira <jcpereira070@gmail.com>
2020-06-18 20:45:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -40,6 +40,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
+  rainbow_colors = ["wow", "am","Yellow", "Green"]
+  rainbow_colors[0]
+  
   
 end
 
