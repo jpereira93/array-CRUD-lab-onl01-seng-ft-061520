868845f0b36d2ff0e215cf485f96bce0f3865ad8
Jessica Pereira <jcpereira070@gmail.com>
2020-06-18 20:15:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ end
 
 def add_element_to_end_of_array(array,element)
    rainbow_colors = ["Red", "Orange","Yellow", "Green"]
-   rainbow_colors << "Blue!" 
+   rainbow_colors << "array!" 
   
 end
 
