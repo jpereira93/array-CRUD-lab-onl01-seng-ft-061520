3088d9d3153e5a26528e2f6c79d8319022e9205f
Jessica Pereira <jcpereira070@gmail.com>
2020-06-18 20:11:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,6 +15,8 @@ def add_element_to_end_of_array(array,element)
 end
 
 def add_element_to_start_of_array(array)
+  rainbow_colors = ["Red", "Orange","Yellow", "Green"]
+  rainbow_colors.unshift("Blue")
   
 end
 
